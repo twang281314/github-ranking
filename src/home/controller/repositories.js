7b2ba7540038992ecba04c6e393bfa496908b60a
wangtao <wangtao@iscs.com.cn>
2016-10-23 18:57:17 +0800

    get data  from database

--- src/home/controller/repositories.js
+++ src/home/controller/repositories.js
@@ -10,40 +10,49 @@ export default class extends Base {
      */
     async indexAction() {
         //auto render template file index_index.html
-        let that = this;
 
-        let repositories = await this.model("repositories").find();
+        let repositories = await this.model("repositories").select();
+        this.assign('items', repositories);
+        this.assign('title', 'repositories ranking');
+        return this.display();
+    }
+ 
+    /**
+     * 同步数据至数据库
+     */
+    async updateAction() {
+
         const options = {
             url: 'https://api.github.com/search/repositories?q=stars:>1000&page=1&per_page=1000',
             headers: {
                 'User-Agent': 'request'
             }
         };
-        request(options, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
+        // request(options, (error, response, body) => {
+        //     if (!error && response.statusCode == 200) {
 
-                let items = JSON.parse(body).items;
-                // let repositoriesArray = [];
-                // items.forEach((item) => {
-                //     let repository = {};
-                //     repository.id = item.id;
-                //     repository.name = item.name;
-                //     repository.full_name = item.full_name;
-                //     repository.html_url = item.html_url;
-                //     repository.description = item.description;
-                //     repository.stargazers_count = item.stargazers_count;
-                //     repository.watchers_count = item.watchers_count;
-                //     repository.language = item.language;
-                //     repository.forks_count = item.forks_count;
-                //     this.model('repositories').thenAdd(repository, {
-                //         id: repository.id
-                //     });
-                // });
-                // this.model('repositories').addMany(repositoriesArray);
-                this.assign('items', JSON.parse(body).items);
-                this.assign('title', 'repositories ranking');
-            }
-            return this.display();
-        });
+        //         let items = JSON.parse(body).items;
+        //         // let repositoriesArray = [];
+        //         // items.forEach((item) => {
+        //         //     let repository = {};
+        //         //     repository.id = item.id;
+        //         //     repository.name = item.name;
+        //         //     repository.full_name = item.full_name;
+        //         //     repository.html_url = item.html_url;
+        //         //     repository.description = item.description;
+        //         //     repository.stargazers_count = item.stargazers_count;
+        //         //     repository.watchers_count = item.watchers_count;
+        //         //     repository.language = item.language;
+        //         //     repository.forks_count = item.forks_count;
+        //         //     this.model('repositories').thenAdd(repository, {
+        //         //         id: repository.id
+        //         //     });
+        //         // });
+        //         // this.model('repositories').addMany(repositoriesArray);
+        //         this.assign('items', JSON.parse(body).items);
+        //         this.assign('title', 'repositories ranking');
+        //     }
+        //     return this.display();
+        // });
     }
 }
