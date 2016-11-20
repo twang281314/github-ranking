2e3a30570a1cb562700ad68a8dd8abe81ae7b378
王涛 <wangtao@iscs.com.cn>
2016-11-20 09:32:38 +0800

    update  job

--- src/common/bootstrap/schedulejob.js
+++ src/common/bootstrap/schedulejob.js
@@ -12,21 +12,21 @@
 
 import schedule from 'node-schedule';
 
-let jobId = schedule.scheduleJob(' 59 * * * *', () => {
+let jobId = schedule.scheduleJob(' 59 23 * * *', () => {
     //console.log(Date() + ":It's been 2  hours!");
     //调用一个 Action
     think.http("/repositories/update", true); //模拟访问
 
 });
 
-let jobUser = schedule.scheduleJob(' 10 * * * *', () => {
+let jobUser = schedule.scheduleJob(' 10 23 * * *', () => {
 
     think.http("/users/update", true);
 
 });
 
 
-let jobUpdateUser = schedule.scheduleJob(' 25 * * * *', () => {
+let jobUpdateUser = schedule.scheduleJob(' 25 23 * * *', () => {
 
     think.http("/users/updateuser", true);
 
