7fcbd338a6e7302d9f4a66a5541d12b5072a803d
王涛 <wangtao@iscs.com.cn>
2016-12-29 14:43:49 +0800

    sort

--- src/api/controller/repositories.js
+++ src/api/controller/repositories.js
@@ -13,7 +13,11 @@ export default class extends Base {
     async listAction() {
         var pageCurrent = this.post('pageCurrent') || 1;
         var pageSize = this.post('pageSize') || 20;
+        var sortField = this.post('sortField') || 'stargazers_count'; //排序字段
+        var sortOrder = this.post('sortOrder') || 'descend'; //ascend升序 descend降序
+        sortOrder = (sortOrder == 'ascend' ? 'asc' : 'desc');
         let repositories = await this.model('repositories')
+            .order(sortField+' '+sortOrder)
             .page(pageCurrent, pageSize)
             .countSelect();
         this.header("Access-Control-Allow-Origin", this.header("origin") || "*");
--- src/api/controller/users.js
+++ src/api/controller/users.js
@@ -3,7 +3,7 @@
  * @Description:  users api 
  * @Date: 2016-12-23 14:35:17 
  * @Last Modified by: anytao
- * @Last Modified time: 2016-12-29 13:54:15
+ * @Last Modified time: 2016-12-29 14:43:22
  */
 
 'use strict';
@@ -16,7 +16,11 @@ export default class extends Base {
     async listAction() {
         var pageCurrent = this.post('pageCurrent') || 1;
         var pageSize = this.post('pageSize') || 20;
+        var sortField = this.post('sortField') || 'followers'; //排序字段
+        var sortOrder = this.post('sortOrder') || 'descend'; //ascend升序 descend降序
+        sortOrder = (sortOrder == 'ascend' ? 'asc' : 'desc');
         let users = await this.model('users')
+            .order(sortField + ' ' + sortOrder)
             .page(pageCurrent, pageSize)
             .countSelect();
         this.header("Access-Control-Allow-Origin", this.header("origin") || "*");
