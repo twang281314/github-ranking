33b7ed85afaa46738a5a071ffe2f95a83c4b78b7
anytao <twang281314@gmail.com>
2017-11-03 17:46:48 +0800

    ignore get method

--- src/controller/base.js
+++ src/controller/base.js
@@ -1,6 +1,10 @@
 module.exports = class extends think.Controller {
   __before() {
 
+    if (this.isGet) {
+      return true;
+    }
+
     // const token = this.header();
     let systemToken = this.config('token');
     let token = this.header('token'); //获取 header
