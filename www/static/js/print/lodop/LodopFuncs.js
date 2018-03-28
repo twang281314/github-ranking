18d32b54391696c91299542ec294c44b131b2119
anytao <twang281314@gmail.com>
2018-03-28 17:20:49 +0800

    :sunny: lodop use https

--- www/static/js/print/lodop/LodopFuncs.js
+++ www/static/js/print/lodop/LodopFuncs.js
@@ -39,13 +39,13 @@ function needCLodop(){
 if (needCLodop()) {
     var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     var oscript = document.createElement("script");
-    oscript.src ="http://localhost:8000/CLodopfuncs.js?priority=1";
+    oscript.src ="https://localhost:8443/CLodopfuncs.js?priority=1";
     head.insertBefore( oscript,head.firstChild );
 
-    //引用双端口(8000和18000）避免其中某个被占用：
-    oscript = document.createElement("script");
-    oscript.src ="http://localhost:18000/CLodopfuncs.js?priority=0";
-    head.insertBefore( oscript,head.firstChild );
+    // //引用双端口(8000和18000）避免其中某个被占用：
+    // oscript = document.createElement("script");
+    // oscript.src ="http://localhost:18000/CLodopfuncs.js?priority=0";
+    // head.insertBefore( oscript,head.firstChild );
 };
 
 //====获取LODOP对象的主过程：====
