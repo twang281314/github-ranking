9d3be91fc96af9c26f85bf122a3743245ee2c064
anytao <twang281314@gmail.com>
2018-05-28 21:42:42 +0800

    update api

---
+++ src/controller/api.js
@@ -0,0 +1,28 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async indexAction() {
+    return this.json({
+        msg:'hello world'
+    })
+  }
+  
+  /**
+   * 接受支付宝异步通知结果
+   */
+  async receiveNotifyAction(){
+
+    let out_trade_no = this.post('out_trade_no')
+    let trade_no = this.post('trade_no')
+
+    think.logger.info(this.ctx)
+
+    return this.json({
+        data:{
+            out_trade_no:out_trade_no,
+            trade_no:trade_no
+        }
+    })
+    
+  }
+};
