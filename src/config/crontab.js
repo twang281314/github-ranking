cba13c938a85877823a77242ec65823059769ace
anytao <twang281314@gmail.com>
2019-05-24 16:06:47 +0800

    update

--- src/config/crontab.js
+++ src/config/crontab.js
@@ -5,16 +5,16 @@ module.exports = [{
 }, {
     cron: '59 23 * * *',
     handle: '/users/update',
-    immediate: true,
+    immediate: false,
     type: 'all'
 },{
     cron: '10 23 * * *',
     handle: '/repositories/update',
-    immediate: true,
+    immediate: false,
     type: 'all'
 },{
     cron: '25 23 * * *',
     handle: '/users/updateuser',
-    immediate: true,
+    immediate: false,
     type: 'all'
 }]
